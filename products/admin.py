4a2cff57de106590248c91f580adf30a5293322e
Paul Kenny <redpaulkenny@gmail.com>
2020-01-07 13:31:22 +0000

    Add products app, define basic models

---
+++ products/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import VIPSlot, Kit
+# Register your models here.
+
+admin.site.register(VIPSlot)
+admin.site.register(Kit)
---
+++ products/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class VIPSlot(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    for_server = models.CharField(max_length=300)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2, blank=False, null=False, default=5)
+
+    def __str__(self):
+        return self.title
+
+
+class Kit(models.Model):
+    slug = models.SlugField()
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    for_server = models.CharField(max_length=300)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2, blank=False, null=False, default=5)
+    allowed_vips = models.ManyToManyField(VIPSlot)
+
+    def __str__(self):
+        return self.title
