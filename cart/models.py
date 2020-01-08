6fcff6d123cf8b0750b41d5a840b950c1e337d87
Paul Kenny <redpaulkenny@gmail.com>
2020-01-08 19:25:34 +0000

    Define cart and order models, make migrations

--- cart/models.py
+++ cart/models.py
@@ -1,3 +1,25 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from products.models import Product
+
+User = get_user_model()
 
 # Create your models here.
+class Cart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.SmallIntegerField(default=1)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.quantity} of {self.item.name}'
+
+
+class Order(models.Model):
+    orderitems = models.ManyToManyField(Cart)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.user.username
