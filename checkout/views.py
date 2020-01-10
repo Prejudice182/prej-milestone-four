07a333e6942c6093516151e06084e2f3b9f58ba3
Paul Kenny <redpaulkenny@gmail.com>
2020-01-10 16:29:11 +0000

    Add checkout view, add urls for checkout

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,21 @@
 from django.shortcuts import render
+from .forms import BillingForm
+from cart.models import Order
 
 # Create your views here.
+def checkout(request):
+    form = BillingForm
+
+    order_qs = Order.objects.filter(user=request.user, ordered=False)
+    order = order_qs.first()
+
+    order_items = order.orderitems.all()
+    order_total = order.get_totals()
+
+    context = {
+        'form': form,
+        'order_items': order_items,
+        'order_total': order_total
+    }
+
+    return render(request, 'checkout/index.html', context)
