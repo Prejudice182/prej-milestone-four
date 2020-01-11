01e93e6723eceecb444bc056895413f6d0cc99b1
Paul Kenny <redpaulkenny@gmail.com>
2020-01-11 23:15:55 +0000

    Move home page into pages app, change url to match

--- rustfiends/urls.py
+++ rustfiends/urls.py
@@ -17,9 +17,10 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-from django.views.generic.base import TemplateView
+from pages.views import home
+
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'), name='home'),
+    path('', home, name='home'),
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('users/', include('django.contrib.auth.urls')),
