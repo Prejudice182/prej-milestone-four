54aac263fe0744049f10d5f4142148e2b0d3eaec
Paul Kenny <redpaulkenny@gmail.com>
2020-01-16 19:00:14 +0000

    Alter category, add slugify method to products

--- products/models.py
+++ products/models.py
@@ -1,17 +1,18 @@
 from django.db import models
+from django.template.defaultfilters import slugify
 
 # Create your models here.
 class Category(models.Model):
     slug = models.SlugField()
     title = models.CharField(max_length=300)
-    primaryCategory = models.BooleanField(default=False)
+    featured = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
 
 
 class Product(models.Model):
-    slug = models.SlugField()
+    slug = models.SlugField(editable=False)
     name = models.CharField(max_length=300)
     mainimage = models.ImageField(upload_to='products', blank=True)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
@@ -21,3 +22,9 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+    def save(self, *args, **kwargs):
+        if not self.id:
+            self.slug = slugify(self.name)
+
+        super(Product, self).save(*args, **kwargs)
