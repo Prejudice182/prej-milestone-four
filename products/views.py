aee6278a591cdbb0629c559c78997a3ee4609021
Paul Kenny <redpaulkenny@gmail.com>
2020-01-16 20:54:27 +0000

    Add pagination to category view

--- products/views.py
+++ products/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
+from django.views.generic.detail import SingleObjectMixin
 from .models import Product, Category
 
 # Create your views here.
@@ -8,9 +9,18 @@ class Home(ListView):
     template_name = 'products/home.html'
 
 
-class CategoryView(ListView):
-    template_name = 'products/home.html'
-    
+class CategoryView(SingleObjectMixin, ListView):
+    template_name = 'products/category.html'
+    paginate_by = 6
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object(queryset=Category.objects.all())
+        return super().get(request, *args, **kwargs)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['category'] = self.object
+        return context
+
     def get_queryset(self):
-        self.category = get_object_or_404(Category, slug=self.kwargs['slug'])
-        return Product.objects.filter(category=self.category)
+        return self.object.product_set.all()
