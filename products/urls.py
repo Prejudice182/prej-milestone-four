4b49d07bc65f21710f31b29b0eee3de3544d135a
Paul Kenny <redpaulkenny@gmail.com>
2020-01-16 19:28:58 +0000

    Add category view and url, wire to navbar

--- products/urls.py
+++ products/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import Home
+from .views import Home, CategoryView
 app_name = 'products'
 
 urlpatterns = [
     path('', Home.as_view(), name='home'),
+    path('category/<slug>/', CategoryView.as_view(), name='category'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,7 +1,16 @@
+from django.shortcuts import get_object_or_404
 from django.views.generic import ListView
-from .models import Product
+from .models import Product, Category
 
 # Create your views here.
 class Home(ListView):
     model = Product
     template_name = 'products/home.html'
+
+
+class CategoryView(ListView):
+    template_name = 'products/home.html'
+    
+    def get_queryset(self):
+        self.category = get_object_or_404(Category, slug=self.kwargs['slug'])
+        return Product.objects.filter(category=self.category)
