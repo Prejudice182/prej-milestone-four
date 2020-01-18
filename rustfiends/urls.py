a8dde3f40a50c2cf3e32010b93d767e05909b970
Paul Kenny <redpaulkenny@gmail.com>
2020-01-18 19:36:23 +0000

    Setup dev and prod serving of files

--- rustfiends/urls.py
+++ rustfiends/urls.py
@@ -16,6 +16,7 @@ Including another URLconf
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.urls import path, include
 from pages.views import HomeView
 
@@ -28,5 +29,7 @@ urlpatterns = [
     path('cart/', include('cart.urls', namespace='cart')),
     path('checkout/', include('checkout.urls', namespace='checkout')),
     path('pages/', include('pages.urls', namespace='pages')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) 
+]
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += staticfiles_urlpatterns()
