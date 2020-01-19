96f510c0b88d35fd03c9733f86faa8cb82490767
Paul Kenny <redpaulkenny@gmail.com>
2020-01-19 21:09:09 +0000

    Fetch featured items to home page, add to template

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,18 @@
-from django.views.generic import TemplateView
+from django.db.models import OuterRef, Subquery
+from django.views.generic import ListView
+from products.models import Category, Product
 
 # Create your views here.
-class HomeView(TemplateView):
+
+
+class HomeView(ListView):
     template_name = 'pages/home.html'
+    queryset = Category.objects.filter(featured=True)
+    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        categories = Category.objects.filter(featured=True)
+        context['featured'] = []
+        for category in categories:
+            context['featured'] += category.product_set.all().order_by('?')[:1]
+        return context
