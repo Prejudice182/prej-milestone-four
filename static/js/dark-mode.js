cb9e5077b7888839c4cb9f86924ccd85555eef0b
Paul Kenny <redpaulkenny@gmail.com>
2020-01-21 18:30:54 +0000

    Add sticky footer and nav, remove darkmode for now

--- static/js/dark-mode.js
+++ static/js/dark-mode.js
@@ -1,19 +1,19 @@
-($ => {
+// ($ => {
 
-const darkMode = (isDark) => {
-    if (isDark.matches) {
-        $('.navbar').removeClass('navbar-light bg-light').addClass('navbar-dark bg-dark');
-        console.log('darkmode on');
-    } else {
-        $('.navbar').addClass('navbar-light bg-light').removeClass('navbar-dark bg-dark');
-        console.log('darkmode off')
-    }
-};
+// const darkMode = (isDark) => {
+//     if (isDark.matches) {
+//         $('.navbar').removeClass('navbar-light bg-light').addClass('navbar-dark bg-dark');
+//         console.log('darkmode on');
+//     } else {
+//         $('.navbar').addClass('navbar-light bg-light').removeClass('navbar-dark bg-dark');
+//         console.log('darkmode off')
+//     }
+// };
 
-let isDark = window.matchMedia('(prefers-color-scheme: dark)');
+// let isDark = window.matchMedia('(prefers-color-scheme: dark)');
 
-darkMode(isDark);
+// darkMode(isDark);
 
-isDark.addListener(darkMode);
+// isDark.addListener(darkMode);
 
-})(jQuery);
+// })(jQuery);
