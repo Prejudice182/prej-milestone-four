9df24ebc391d2b689b08d7b6694d9f08f7ca2ac8
Paul Kenny <redpaulkenny@gmail.com>
2020-01-24 01:24:17 +0000

    Add crispy form layout, wire in template

--- products/forms.py
+++ products/forms.py
@@ -1,9 +1,13 @@
 from django import forms
+from crispy_forms.bootstrap import StrictButton, FieldWithButtons
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Field
 
 
 class QuantitySelectForm(forms.Form):
     quantity = forms.IntegerField(min_value=1, max_value=10, widget=forms.Select(
         choices=[(i, i) for i in range(1, 11)]))
+
 
 class OrderByForm(forms.Form):
     ORDER_BY_CHOICES = [
@@ -14,5 +18,13 @@ class OrderByForm(forms.Form):
         ('pk', 'Id: Ascending'),
         ('pkd', 'Id: Descending'),
     ]
-    order = forms.ChoiceField(choices=ORDER_BY_CHOICES, label='Sort Products', required=False)
-    
+    order = forms.ChoiceField(choices=ORDER_BY_CHOICES,
+                              required=False, label='')
+
+    def __init__(self, *args, **kwargs):
+        super(OrderByForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_class = 'form-inline justify-content-center justify-content-sm-end'
+        self.helper.form_method = 'get'
+        self.helper.layout = Layout(FieldWithButtons(
+            'order', StrictButton('Sort', type='submit', css_class='btn-primary')))
