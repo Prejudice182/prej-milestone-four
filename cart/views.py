3c36f9ac2992ecfa0f60f1d0140bfb83afa4a119
Paul Kenny <redpaulkenny@gmail.com>
2020-01-26 14:48:24 +0000

    Rewrite views to remove repeated code blocks

--- cart/views.py
+++ cart/views.py
@@ -4,66 +4,56 @@ from django.shortcuts import render, redirect, get_object_or_404
 from .models import Cart, CartItem
 from products.models import Product
 
-# Create your views here.
+def add_func(request, cart_item, exists=False):
+    if exists:
+        if request.method == 'POST':
+            cart_item.quantity += int(request.POST['quantity'])
+        else:
+            cart_item.quantity += 1
+    else:
+        if request.method == 'POST':
+            cart_item.quantity = int(request.POST['quantity'])
+        else:
+            cart_item.quantity = 1
+    cart_item.save()
+
+def dec_or_remove(cart, product, remove=False):
+    contents = cart.items.all()
+    if contents.exists():
+        item_in_cart = contents.filter(product=product)
+        if item_in_cart.exists():
+            item = item_in_cart.first()
+            if remove or item.quantity == 1:
+                item.delete()
+            else:
+                item.quantity -= 1
+                item.save()
 
 
 def add_to_cart(request, slug):
     product = get_object_or_404(Product, slug=slug)
     cart, created = Cart.objects.get_or_create(
         customer=request.user, ordered=False)
+    item_in_cart = cart.items.filter(product=product)
 
-    cart_item_qs = CartItem.objects.filter(cart=cart, product=product)
-    if cart_item_qs.exists():
-        cart_item = cart_item_qs.first()
-        if request.method == 'POST':
-            cart_item.quantity += int(request.POST['quantity'])
-        else:
-            cart_item.quantity += 1
-        cart_item.save()
-        messages.info(request, 'This item quantity was updated.')
-    else:
+    if created or not item_in_cart.exists():
         cart_item = CartItem.objects.create(cart=cart, product=product)
-        if request.method == 'POST':
-            cart_item.quantity = int(request.POST['quantity'])
-        else:
-            cart_item.quantity = 1
-        cart_item.save()
-        messages.info(request, 'This item was added to your cart.')
+        add_func(request, cart_item)
+    else:
+        cart_item = item_in_cart.first()
+        add_func(request, cart_item, exists=True)
+
     return redirect('cart:home')
 
 
 def decrease_cart(request, slug):
     product = get_object_or_404(Product, slug=slug)
     cart_qs = Cart.objects.filter(customer=request.user, ordered=False)
-
+    
     if cart_qs.exists():
         cart = cart_qs.first()
-        cart_contents_qs = CartItem.objects.filter(cart=cart)
-        if cart_contents_qs.exists():
-            line_count = cart_contents_qs.count()
-            item_qs = cart_contents_qs.filter(product=product)
-            if item_qs.exists():
-                item = item_qs.first()
-                if item.quantity > 1:
-                    item.quantity -= 1
-                    item.save()
-                    messages.info(
-                        request, f'{product.name} quantity was decreased by 1.')
-                else:
-                    item.delete()
-                    messages.info(
-                        request, f'{product.name} was removed from your cart.')
-                    if line_count == 1:
-                        cart.delete()
-                        messages.info(request, 'Your cart is now empty.')
-                        return redirect('products:home')
-            else:
-                messages.warning(
-                    request, f'{product.name} was not found in your cart.')
-                return redirect('products:home')
-    else:
-        messages.warning(request, 'You do not have an active cart.')
-        return redirect('products:home')
+        dec_or_remove(cart, product, remove=False)
+        messages.info(request, f'{product.name} quantity updated!')
 
     return redirect('cart:home')
 
@@ -74,23 +64,10 @@ def remove_from_cart(request, slug):
 
     if cart_qs.exists():
         cart = cart_qs.first()
-        cart_item_qs = CartItem.objects.filter(cart=cart, product=product)
-        if cart_item_qs.exists():
-            cart_item = cart_item_qs.first()
-            cart_item.delete()
-            messages.info(
-                request, f'{product.name} was removed from your cart.')
-            if cart_item_qs.count() == 1:
-                cart.delete()
-                messages.info(request, 'Your cart is now empty.')
-                return redirect('products:home')
-        else:
-            messages.warning(
-                request, f'{product.name} was not found in your cart.')
-    else:
-        messages.warning(request, 'You do not have anything in your cart.')
+        dec_or_remove(cart, product, remove=True)
+        messages.info(request, f'{product.name} removed!')
 
-    return redirect('products:home')
+    return redirect('cart:home')
 
 
 @login_required(login_url='/users/login/')
@@ -100,12 +77,14 @@ def cart_view(request):
     carts = Cart.objects.filter(customer=user, ordered=False)
 
     if carts.exists():
-        cart_items = CartItem.objects.filter(cart=carts.first())
-        context = {
-            'cart': carts.first(),
-            'cart_items': cart_items,
-        }
-        return render(request, 'cart/home.html', context)
-    else:
-        messages.warning(request, 'You do not have an active order')
-        return redirect('products:home')
+        cart = carts.first()
+        cart_items = CartItem.objects.filter(cart=cart)
+        if cart_items.exists():
+            context = {
+                'cart': carts.first(),
+                'cart_items': cart_items,
+            }
+            return render(request, 'cart/home.html', context)
+            
+    messages.warning(request, 'You do not have an active order')
+    return redirect('products:home')
