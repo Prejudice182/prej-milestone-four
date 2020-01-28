ad848c084ab3201c51c839dd0aa6c65eff9b14ed
Paul Kenny <redpaulkenny@gmail.com>
2020-01-28 12:22:37 +0000

    Add stripe URLS to settings, add to URLconf

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import checkout, payment, charge, all_orders
+from .views import checkout, payment, confirm, all_orders
 app_name = 'checkout'
 
 urlpatterns = [
     path('', checkout, name='home'),
     path('payment/', payment, name='payment'),
-    path('charge/', charge, name='charge'),
+    path('confirm/<session_id>', confirm, name='confirm'),
     path('all-orders/', all_orders, name='all_orders'),
 ]
