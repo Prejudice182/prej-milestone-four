6e06e0d7555bf0b38316bfdf4db664c12337d795
Paul Kenny <redpaulkenny@gmail.com>
2020-01-29 19:12:16 +0000

    Add comments to Pages app, prefetch related set

--- pages/views.py
+++ pages/views.py
@@ -5,12 +5,16 @@ from products.models import Category
 
 
 class HomeView(ListView):
+    '''
+    Home Page view, using a list view to display the featured categories
+    '''
     template_name = 'pages/home.html'
     queryset = Category.objects.filter(featured=True)
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        categories = Category.objects.filter(featured=True)
+        # Get 3 featured categories
+        categories = Category.objects.filter(featured=True).order_by('?').prefetch_related('product_set')[:3]
         context['featured'] = []
         for category in categories:
             context['featured'] += category.product_set.all().order_by('?')[:1]
