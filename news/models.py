cdd08a821d17ec688cefa0eb70ec193269e858d9
Paul Kenny <redpaulkenny@gmail.com>
2020-01-30 01:57:21 +0000

    Add tagline and migrate

--- news/models.py
+++ news/models.py
@@ -7,7 +7,11 @@ User = get_user_model()
 class Article(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE)
     headline = models.CharField(max_length=100)
+    tag_line = models.CharField(max_length=100)
     content = models.TextField()
     banner_image = models.ImageField(upload_to='media/news/', blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.headline
