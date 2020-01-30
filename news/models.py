8ef9f10ac7bd09d98fe74336e6feb4fc931c30d2
Paul Kenny <redpaulkenny@gmail.com>
2020-01-30 01:51:14 +0000

    Create article model, add to admin

--- news/models.py
+++ news/models.py
@@ -1,3 +1,13 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
+User = get_user_model()
+
 # Create your models here.
+class Article(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    headline = models.CharField(max_length=100)
+    content = models.TextField()
+    banner_image = models.ImageField(upload_to='media/news/', blank=True)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
