a37251bde1e744929030c021a524ba7ca9461096
Paul Kenny <redpaulkenny@gmail.com>
2020-01-30 23:49:19 +0000

    Add more tests

--- cart/tests.py
+++ cart/tests.py
@@ -1,8 +1,9 @@
 from django.contrib.auth import get_user_model
-from django.test import TestCase
+from django.test import TestCase, Client, RequestFactory
 from django.urls import reverse, resolve
 from .views import add_to_cart, decrease_cart, remove_from_cart, cart_view
 from .models import Cart, CartItem
+from products.models import Category, Product
 import datetime
 
 User = get_user_model()
@@ -42,3 +43,26 @@ class TestModels(TestCase):
         cart = Cart(customer=user)
 
         self.assertEqual(cart.ordered, False)
+
+class TestViews(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def test_response_logged_out_cart(self):
+        response = self.client.get('/cart/')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response.url, '/users/login/?next=/cart/')
+
+    def test_response_logged_in_cart(self):
+        self.factory = RequestFactory()
+        self.user = User.objects.create_user(username='TestUser', email='test@mail.com', password='testing1234')
+
+        self.client.login(username='TestUser', password='testing1234')
+        
+        category = Category(slug='medical', title='Medical')
+        category.save()
+        product = Product(slug='bandage', name='Bandage', category=category, preview_text='test', detail_text='test', mainimage='blank.jpg')
+        product.save()
+
+        response = self.client.get(reverse('cart:add', args=['bandage']))
+        self.assertEqual(response.url, '/cart/')
--- checkout/tests.py
+++ checkout/tests.py
@@ -1,3 +1,18 @@
 from django.test import TestCase
-
+from django.urls import reverse, resolve
+from .models import BillingAddress, Order
+from .views import checkout, payment, all_orders
 # Create your tests here.
+
+class TestUrls(TestCase):
+    def test_checkout_url(self):
+        url = reverse('checkout:home')
+        self.assertEqual(resolve(url).func, checkout)
+
+    def test_payment_url(self):
+        url = reverse('checkout:payment')
+        self.assertEqual(resolve(url).func, payment)
+
+    def test_orders_url(self):
+        url = reverse('checkout:all_orders')
+        self.assertEqual(resolve(url).func, all_orders)
