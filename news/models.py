ca7010886a2304e84e6170fe55adef0cc3486233
Paul Kenny <redpaulkenny@gmail.com>
2020-01-30 15:46:22 +0000

    Add absolute url to model

--- news/models.py
+++ news/models.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse
 
 User = get_user_model()
 
@@ -12,6 +13,9 @@ class Article(models.Model):
     banner_image = models.ImageField(upload_to='media/news/', blank=True)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def get_absolute_url(self):
+        return reverse('news:details', kwargs={'pk': self.pk})
 
     def __str__(self):
         return self.headline
