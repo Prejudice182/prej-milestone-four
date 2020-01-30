b55846e000920657e0fde686c89ba70c2078872f
Paul Kenny <redpaulkenny@gmail.com>
2020-01-30 14:14:12 +0000

    Add news list view and template

--- news/urls.py
+++ news/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import NewsHomeView
 app_name = 'news'
 
 urlpatterns = [
+    path('', NewsHomeView.as_view(), name='home'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Article
 
 # Create your views here.
+
+
+class NewsHomeView(ListView):
+    paginate_by = 10
+    template_name = 'news/index.html'
+    queryset = Article.objects.all().order_by('-updated')
+    context_object_name = 'articles'
