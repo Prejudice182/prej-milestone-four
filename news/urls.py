4e5bd9091f801731a8c3b2b21e07aad0e7803a40
Paul Kenny <redpaulkenny@gmail.com>
2020-01-30 15:46:57 +0000

    Add create and detail view, add to urlconf

--- news/urls.py
+++ news/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
-from .views import NewsHomeView
+from .views import NewsHomeView, NewsCreateView, NewsDetailView
 app_name = 'news'
 
 urlpatterns = [
     path('', NewsHomeView.as_view(), name='home'),
+    path('create/', NewsCreateView.as_view(), name='create'),
+    path('details/<pk>', NewsDetailView.as_view(), name='details'),
 ]
--- news/views.py
+++ news/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView
 from .models import Article
 
 # Create your views here.
@@ -10,3 +12,18 @@ class NewsHomeView(ListView):
     template_name = 'news/index.html'
     queryset = Article.objects.all().order_by('-updated')
     context_object_name = 'articles'
+
+
+class NewsCreateView(LoginRequiredMixin, CreateView):
+    model = Article
+    fields = ['headline', 'tag_line', 'banner_image', 'content']
+    template_name = 'news/create.html'
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+class NewsDetailView(DetailView):
+    model = Article
+    template_name = 'news/details.html'
+    context_object_name = 'article'
