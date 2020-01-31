7d635bca93eb81f3884cbcdd8582a4eb6beabbad
Paul Kenny <redpaulkenny@gmail.com>
2020-01-31 02:21:00 +0000

    Add logo, add to navbar

--- static/js/main.js
+++ static/js/main.js
@@ -1,6 +1,6 @@
 ($ => {
     // Add active class to current page link
-    $('nav a').parents('li,ul').removeClass('active');
+    $('#myNavBar a').parents('li,ul').removeClass('active');
     $('a[href="' + this.location.pathname + '"]').parents('li,ul').addClass('active');
 
     // Add sr-only to current page
