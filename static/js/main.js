5c26da317f98f478e4684a2c369ac346d8a9dcaf
Paul Kenny <redpaulkenny@gmail.com>
2020-01-31 00:31:22 +0000

    Add semicolons to JS

--- static/js/main.js
+++ static/js/main.js
@@ -4,6 +4,6 @@
     $('a[href="' + this.location.pathname + '"]').parents('li,ul').addClass('active');
 
     // Add sr-only to current page
-    $('.sr-only').remove()
-    $('a[href="' + this.location.pathname + '"]').append('<span class="sr-only">(current)</span>')
+    $('.sr-only').remove();
+    $('a[href="' + this.location.pathname + '"]').append('<span class="sr-only">(current)</span>');
 })(jQuery);
